c4696e4bf5b97386c29d45d0361302aa4ab47cad
KevinMendozaG <kevin.ymg@hotmail.com>
2021-03-07 20:14:55 -0500

    CerrarSesion

--- components/account/RegisterForm.js
+++ components/account/RegisterForm.js
@@ -1,9 +1,69 @@
 import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Button, Icon, Input } from 'react-native-elements'
+import { size } from 'lodash'
+import { useNavigation } from "@react-navigation/native";
+
+import { validateEmail } from '../../utils/helpers'
+import { registerUser } from '../../utils/actions'
+import Loading from '../Loading';
 
 export default function RegisterForm() {
     const [showPassword, setShowPassword] = useState(false)
+    const [formData, setFormData] = useState(defaultFormValues())
+    const [errorEmail, setErrorEmail] = useState("")
+    const [errorPassword, setErrorPassword] = useState("")
+    const [errorConfirm, setErrorConfirm] = useState("")
+    const [loading, setLoading] = useState(false)
+
+    const navigation = useNavigation()
+    
+    const onChange = (e, type) => {
+        setFormData({...formData, [type]: e.nativeEvent.text})
+        
+    }
+    const doRegisterUser = async() =>{
+        if (!validateData()){
+            return;
+        }
+
+        setLoading (true)
+        const result = await registerUser(formData.email, formData.password)
+        setLoading(false)
+        if (!result.statusResponse){
+            setErrorEmail(result.error)
+            return
+        }
+
+        navigation.navigate("account")
+    }
+    const validateData = () =>{
+        setErrorConfirm("")
+        setErrorEmail("")
+        setErrorPassword("")
+        let isValid = true
+
+        if(!validateEmail(formData.email)) {
+            setErrorEmail("Debes de ingresar un email valido.")
+            isValid = false
+        }
+
+        if(size(formData.password) < 6){
+            setErrorPassword("Debes ingresar una contraseña de al menos seis caracteres")
+            isValid = false
+        }
+
+        if(size(formData.confirm) < 6){
+            setErrorConfirm("Debes ingresar una confirmación de contraseña de al menos seis caracteres")
+            isValid = false
+        }
+        if(formData.password !== formData.confirm){
+            setErrorPassword("La contraseña y la confirmacion no son iguales")
+            setErrorConfirm("La contraseña y la confirmacion no son iguales")
+            isValid = false
+        }
+        return isValid
+    }
 
     return (
         <View style={styles.form}>
@@ -11,12 +71,18 @@ export default function RegisterForm() {
                 containerStyle={styles.input}
                 placeholder= "Ingresa tu email..."
                 keyboardType="email-address"
+                onChange= {(e) => onChange(e, "email")}
+                errorMessage={errorEmail}
+                defaultValue={formData.email}
             />
             <Input
                 containerStyle={styles.input}
                 placeholder= "Ingresa tu contraseña..."
                 password ={true}
                 secureTextEntry={!showPassword}
+                onChange= {(e) => onChange(e, "password")}
+                errorMessage={errorPassword}
+                defaultValue={formData.password}
                 rightIcon={
                     <Icon
                         type= "material-community"
@@ -31,6 +97,9 @@ export default function RegisterForm() {
                 placeholder= "Confirma tu contraseña..."
                 password ={true}
                 secureTextEntry={!showPassword}
+                onChange= {(e) => onChange(e, "confirm")}
+                errorMessage={errorConfirm}
+                defaultValue={formData.confirm}
                 rightIcon={
                     <Icon
                         type= "material-community"
@@ -44,9 +113,17 @@ export default function RegisterForm() {
                 title="Registrar nuevo Usuario"
                 containerStyle={styles.btnContainer}
                 buttonStyle={styles.btn}
+                onPress= {() => doRegisterUser()}
+            />
+            <Loading
+                isVisible= {loading}
+                text="Creando Cuenta..."
             />
         </View>
     )
+}
+const defaultFormValues = () => {
+    return { email: "", password: "", confirm: ""} 
 }
 
 const styles = StyleSheet.create({
